b759900168b3e6133087e4ab28dcc4403a6da695
LeonardoKruss <leo.kruss@gmail.com>
2022-08-11 12:04:11 -0300

    Middlewares

--- routes/web.php
+++ routes/web.php
@@ -20,16 +20,26 @@ Route::get('/', function () {
 });
 */
 
-Route::get('/', 'PrincipalController@principal')->name('site.index')->middleware(LogAcessoMiddleware::class);
-Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
-Route::get('/contato', 'ContatoController@contato')->name('site.contato');
-Route::post('/contato', 'ContatoController@salvar')->name('site.contato');
-Route::get('/login', function(){ return 'Login'; })->name('site.login');
+Route::get('/', 'PrincipalController@principal')
+    ->name('site.index')
+    ->middleware(LogAcessoMiddleware::class);
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')
+    ->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato')
+    ->name('site.contato')
+    ->middleware(LogAcessoMiddleware::class);
+Route::post('/contato', 'ContatoController@salvar')
+    ->name('site.contato');
+Route::get('/login', function(){ return 'Login'; })
+    ->name('site.login');
 
 Route::prefix('/app')->group(function(){
-    Route::get('/clientes', function(){ return 'Clientes'; })->name('app.clientes');
-    Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
-    Route::get('/produtos', function() { return 'Produtos'; })->name('produtos');
+    Route::get('/clientes', function(){ return 'Clientes'; })
+        ->name('app.clientes');
+    Route::get('/fornecedores', 'FornecedorController@index')
+        ->name('app.fornecedores');
+    Route::get('/produtos', function() { return 'Produtos'; })
+        ->name('produtos');
 });
 
 Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('teste');
